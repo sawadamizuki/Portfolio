af0f801fba968fe5bd1a7de2e93c6565a2b9200a
sawadamizuki <m.sawada3249@gmail.com>
2022-03-15 15:03:55 +0000

    [add]投稿管理用テーブル

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
 
   root to: 'homes#top'
 
+  resources :posts
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_15_141607) do
+ActiveRecord::Schema.define(version: 2022_03_15_144737) do
+
+  create_table "posts", force: :cascade do |t|
+    t.text "name"
+    t.string "image_id"
+    t.text "introduction"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
