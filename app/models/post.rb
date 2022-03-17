84d242eb8789594e30447d226414de7bfd8469a6
sawadamizuki <m.sawada3249@gmail.com>
2022-03-17 08:04:35 +0000

    [add]いいね機能

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,10 @@ class Post < ApplicationRecord
   belongs_to :user
   attachment :image
   has_many :post_comments, dependent: :destroy
+   has_many :likes, dependent: :destroy
+
+  def liked_by?(user)
+    likes.where(user_id: user.id).exists?
+  end
 
 end
